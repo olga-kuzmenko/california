05aab49b2ac45621ec46a813dfb105bfe9172fb9
olgakuzmenko <olga.kuzmenko.kh@gmail.com>
2023-02-02 10:28:25 +0200

    new section

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+        // autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
 
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     });
